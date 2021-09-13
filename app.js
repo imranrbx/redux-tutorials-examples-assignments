76f78c6d65f07e0597ce647d76788e789400018e
Imran Qasim <imran_qasim@hotmail.com>
2021-09-13 18:47:03 +0500

    Redux JS Part 01 and Assignment Solution for Part 00

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ import { createStore } from "redux";
 const app = document.getElementById("root");
 const increment = document.getElementById("inc");
 const decrement = document.getElementById("dec");
+//these are 3 actions related to counter reducer
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+// these are 3 action creators related to counter reducer
+const incr = (num) => {
+    return { type: "INCREMENT", payload: num };
+};
+const decr = (num) => {
+    return { type: "DECREMENT", payload: num };
+};
+const res = () => {
+    return { type: "RESET" };
+};
 const reset = document.getElementById("reset");
 const counter = (state = 1, action = {}) => {
     switch (action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-        case "RESET":
+        case INCREMENT:
+            return state + action.payload;
+        case DECREMENT:
+            return state - action.payload;
+        case RESET:
             return 1;
         default:
             return state;
@@ -17,17 +31,26 @@ const counter = (state = 1, action = {}) => {
 };
 
 const store = createStore(counter);
-store.subscribe(() => (app.innerText = store.getState()));
+store.subscribe(() => {
+    let state = store.getState();
+    app.innerText = state;
+    state < 10
+        ? increment.removeAttribute("disabled")
+        : increment.setAttribute("disabled", "disabled");
+    state > 1
+        ? decrement.removeAttribute("disabled")
+        : decrement.setAttribute("disabled", "disabled");
+});
 
 window.onload = function () {
     store.dispatch({ type: "INIT" });
 };
 increment.addEventListener("click", () => {
-    store.dispatch({ type: "INCREMENT" });
+    store.getState() < 10 && store.dispatch(incr(2));
 });
 decrement.addEventListener("click", () => {
-    store.dispatch({ type: "DECREMENT" });
+    store.getState() > 1 && store.dispatch(decr(2));
 });
 reset.addEventListener("click", () => {
-    store.dispatch({ type: "RESET" });
+    store.dispatch(res());
 });
