65382c03d28f79ac83dff5290b7dd44c7f0cc0f6
Imran Qasim <imran_qasim@hotmail.com>
2021-10-14 00:19:26 +0500

    Part 14 starting with Redux Toolkit

---
+++ redux-toolkit-tutorial/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            React Redux Toolkit Series
+        </div>
+    )
+}
+
+export default App
---
+++ redux-toolkit-tutorial/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+const el = document.querySelector('#root');
+render(<App />, el);
