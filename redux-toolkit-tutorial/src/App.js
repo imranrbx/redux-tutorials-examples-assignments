b7c7cb1292cf35a90c2faa85abc68536b76ae9a7
Imran Qasim <imran_qasim@hotmail.com>
2021-10-17 12:16:09 +0500

    Part 16 and Part 17 updates

--- redux-toolkit-tutorial/src/App.js
+++ redux-toolkit-tutorial/src/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './store';
+import {
+	increment,
+	decrement,
+	addTodo,
+	remTodo,
+	upTodo,
+} from './store';
 const App = () => {
 	const value = useSelector((state) => state.counter.value);
+	const { todos } = useSelector((state) => state.todos);
+	const [text, setText] = useState('');
 	const dispatch = useDispatch();
 	return (
 		<div>
 			<h1>React Redux Toolkit Series</h1>
 			<h2>{value}</h2>
+			<br />
+			<input
+				type="text"
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+			/>
+			<br />
+			{todos.length > 0 && (
+				<ul>
+					{todos.map((todo, index) => (
+						<li key={todo.id}>
+							{todo.text} -{' '}
+							{todo.completed
+								? 'Completed'
+								: 'Not Completed'}
+							|
+							<button
+								onClick={() =>
+									dispatch(remTodo(todo.id))
+								}>
+								X
+							</button>
+							|
+							<button
+								onClick={() =>
+									dispatch(upTodo(todo.id))
+								}>
+								U
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
+			<button
+				onClick={() => text !== '' && dispatch(addTodo(text))}>
+				Add Todo Item
+			</button>
 			<button
 				onClick={() => {
 					dispatch(increment());
--- redux-toolkit-tutorial/src/store.js
+++ redux-toolkit-tutorial/src/store.js
@@ -1,11 +1,21 @@
-import {
-	configureStore,
-	createAction,
-	createReducer,
-} from '@reduxjs/toolkit';
+import { configureStore, createSlice, nanoid } from '@reduxjs/toolkit';
+//counter action creators
+// export const increment = createAction('counter/increment');
+// export const decrement = createAction('counter/decrement');
+// // todo action creators
+// export const addTodo = createAction('todos/addTodo', (text) => {
+// 	return {
+// 		payload: {
+// 			id: nanoid(),
+// 			text: text,
+// 			completed: false,
+// 			createdAt: new Date().toISOString(),
+// 		},
+// 	};
+// });
+// export const remTodo = createAction('todos/remTodo');
+// export const upTodo = createAction('todos/upTodo');
 
-export const increment = createAction('counter/increment');
-export const decrement = createAction('counter/decrement');
 const logger = (store) => (next) => (action) => {
 	console.log(action, store.getState());
 	next(action);
@@ -21,26 +31,107 @@ const logger = (store) => (next) => (action) => {
 // 		})
 // 		.addDefaultCase((state, action) => {});
 // });
+// const todosReducer = createReducer({ todos: [] }, (builder) => {
+// 	builder
+// 		.addCase(addTodo, (state, action) => {
+// 			const found = state.todos.filter(
+// 				(todo) =>
+// 					todo.text.toLowerCase() ===
+// 					action.payload.text.toLowerCase()
+// 			);
+// 			if (found.length === 0) state.todos.push(action.payload);
+// 		})
+// 		.addCase(remTodo, (state, action) => {
+// 			state.todos = state.todos.filter(
+// 				(todo) => todo.id !== action.payload
+// 			);
+// 		})
+// 		.addCase(upTodo, (state, action) => {
+// 			state.todos = state.todos.map((todo) =>
+// 				todo.id === action.payload
+// 					? { ...todo, completed: !todo.completed }
+// 					: todo
+// 			);
+// 		})
+// 		.addDefaultCase((state, action) => {});
+// });
 // this is another way to create Reducer with Object Notation
 
-const countReducer = createReducer(
-	{ value: 1 },
-	{
-		[increment]: (state, action) => {
-			state.value++;
+// const countReducer = createReducer(
+// 	{ value: 1 },
+// 	{
+// 		[increment]: (state, action) => {
+// 			state.value++;
+// 		},
+// 		[decrement]: (state, action) => {
+// 			state.value--;
+// 		},
+// 	}
+// );
+const counter = createSlice({
+	name: 'counter',
+	initialState: { value: 1 },
+	reducers: {
+		increment: (state) => {
+			state.value += 1;
 		},
-		[decrement]: (state, action) => {
-			state.value--;
+		decrement: (state) => {
+			state.value -= 1;
 		},
-	}
-);
+	},
+});
+const todos = createSlice({
+	name: 'todos',
+	initialState: { todos: [] },
+	reducers: {
+		addTodo: {
+			reducer: (state, action) => {
+				const found = state.todos.filter(
+					(todo) =>
+						todo.text.toLowerCase() ===
+						action.payload.text.toLowerCase()
+				);
+				if (found.length === 0)
+					state.todos.push(action.payload);
+			},
+
+			prepare: (text) => {
+				const id = nanoid();
+				return {
+					payload: {
+						id,
+						text,
+						comp0leted: false,
+						createdAt: new Date().toISOString(),
+					},
+				};
+			},
+		},
+
+		remTodo: (state, action) => {
+			state.todos = state.todos.filter(
+				(todo) => todo.id !== action.payload
+			);
+		},
+		upTodo: (state, action) => {
+			state.todos = state.todos.map((todo) =>
+				todo.id === action.payload
+					? { ...todo, completed: !todo.completed }
+					: todo
+			);
+		},
+	},
+});
 const rootReducer = {
 	reducer: {
-		counter: countReducer,
+		counter: counter.reducer,
+		todos: todos.reducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware().concat(logger),
 	devTools: process.env.NODE_ENV !== 'production',
 };
 const store = configureStore(rootReducer);
+export const { increment, decrement } = counter.actions;
+export const { addTodo, remTodo, upTodo } = todos.actions;
 export default store;
